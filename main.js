afd9e791189bc4fe68305174b97449f5be0f7e48
Shevaitverma <shevaitverma333@gmail.com>
2023-09-12 23:40:17 +0530

    Initializing graphQL server and write some basic code

---
+++ main.js
@@ -0,0 +1,13 @@
+const {ApolloServer} = require("apollo-server");
+const {typeDefs} = require("./schema/type-defs");
+const {resolvers} = require("./schema/resolvers");
+
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+
+
+server.listen()
+    .then(({url}) => {
+        console.log(`Api is running at: ${url}`);
+    });
